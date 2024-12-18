ec5dce62f3371b0b223af436e146688675fd2eae
Michael Variny <michaelvariny@yahoo.com>
2024-12-18 16:27:36 -0500

    starting closer

--- LaunchScripts/Run_Full_Bez_Bypass.sh
+++ LaunchScripts/Run_Full_Bez_Bypass.sh
@@ -3,7 +3,7 @@ subscenario="NotSingle"
 t=17000
 exptype="SuperTest"
 expnum=0
-for (( spacing=311; spacing>=8; spacing-=20 ))
+for (( spacing=104; spacing>=8; spacing-=8 ))
     do
     ((expnum++))
     python3 ../AircraftSpacing.py -s1 $scenario -s2 $subscenario -d $spacing -t $t -en $expnum -et $exptype
